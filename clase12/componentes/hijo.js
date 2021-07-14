eb0e4cef41be1badda7221a4e00c4afe0193a97c
Ezequiel Agustin Estevez <contacto@ezequielestevez.com>
2021-07-14 16:20:45 -0300

    Comunicacion entre componentes indent

--- clase12/componentes/hijo.js
+++ clase12/componentes/hijo.js
@@ -1,9 +1,9 @@
 Vue.component('hijo', {
     template: //html
-    `
+        `
     <div class="py-5 bg-success">
         <h4>Componente Hijo: {{numero}}</h4>
     </div>
     `,
-    props:['numero'] //recibimos informacion
+    props: ['numero'] //recibimos informacion
 })
